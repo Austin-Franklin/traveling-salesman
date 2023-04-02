ef8e576e9a730a406a3ceb283433db631a006146
Austin-Franklin <adf28@students.uwf.edu>
2023-04-01 20:41:50 -0500

    Fixed issues downloading .osm and missing jar

--- src/main/java/OSMDownload.java
+++ src/main/java/OSMDownload.java
@@ -8,24 +8,26 @@ import java.net.*;
  */
 public class OSMDownload {
     public static void osmFile(String location) {
-        String query = String.format(
-            "[out:xml][timeout:25];" +
-            "{{radius=20000}}" +
-            "(" +
-              "node[\"highway\"][\"maxspeed\"](around:{{radius}},{{geocodeCoords:%s}});" +
-              "way[\"highway\"][\"maxspeed\"](around:{{radius}},{{geocodeCoords:%<s}});" +
-              "relation[\"highway\"][\"maxspeed\"](around:{{radius}},{{geocodeCoords:%<s}});" +
-              "node[\"highway\"=\"residential\"](around:{{radius}},{{geocodeCoords:%<s}});" +
-              "way[\"highway\"=\"residential\"](around:{{radius}},{{geocodeCoords:%<s}});" +
-              "relation[\"highway\"=\"residential\"](around:{{radius}},{{geocodeCoords:%<s}});" +
-              "node[\"tiger:name_type\"](around:{{radius}},{{geocodeCoords:%<s}});" +
-              "way[\"tiger:name_type\"](around:{{radius}},{{geocodeCoords:%<s}});" +
-              "relation[\"tiger:name_type\"](around:{{radius}},{{geocodeCoords:%<s}});" +
-            ");" +
-            "(._;>;);" +
-            "out body;"
-            , location);
-            
+        // String query = String.format(
+        //     "[out:xml][timeout:25];\n" +
+        //     "{{radius=20000}}\n" +
+        //     "(\n" +
+        //       "node[\"highway\"][\"maxspeed\"](around:{{radius}},{{geocodeCoords:%s}});\n" +
+        //       "way[\"highway\"][\"maxspeed\"](around:{{radius}},{{geocodeCoords:%<s}});\n" +
+        //       "relation[\"highway\"][\"maxspeed\"](around:{{radius}},{{geocodeCoords:%<s}});\n" +
+        //       "node[\"highway\"=\"residential\"](around:{{radius}},{{geocodeCoords:%<s}});\n" +
+        //       "way[\"highway\"=\"residential\"](around:{{radius}},{{geocodeCoords:%<s}});\n" +
+        //       "relation[\"highway\"=\"residential\"](around:{{radius}},{{geocodeCoords:%<s}});\n" +
+        //       "node[\"tiger:name_type\"](around:{{radius}},{{geocodeCoords:%<s}});\n" +
+        //       "way[\"tiger:name_type\"](around:{{radius}},{{geocodeCoords:%<s}});\n" +
+        //       "relation[\"tiger:name_type\"](around:{{radius}},{{geocodeCoords:%<s}});\n" +
+        //     ");\n" +
+        //     "(._;>;);\n" +
+        //     "out body;"
+        //     , location);
+        String query = """
+            [out:xml][timeout:25];(node["highway"]["maxspeed"](around:20000,30.421309,-87.2169149);way["highway"]["maxspeed"](around:20000,30.421309,-87.2169149);relation["highway"]["maxspeed"](around:20000,30.421309,-87.2169149);node["highway"="residential"](around:20000,30.421309,-87.2169149);way["highway"="residential"](around:20000,30.421309,-87.2169149);relation["highway"="residential"](around:20000,30.421309,-87.2169149);node["tiger:name_type"](around:20000,30.421309,-87.2169149);way["tiger:name_type"](around:20000,30.421309,-87.2169149);relation["tiger:name_type"](around:20000,30.421309,-87.2169149););(._;>;);out body;
+                """;
         try {
             String url = "https://overpass-api.de/api/interpreter?data=" + URLEncoder.encode(query, "UTF-8");
             URLConnection conn = new URL(url).openConnection();
